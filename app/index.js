eaf8840a4170cac96afa171cc70d763b91399f31
Ludwig Muench <muench.ludwig@gmail.com>
2018-02-27 18:42:45 +0100

    add usdt price

--- app/index.js
+++ app/index.js
@@ -6,44 +6,55 @@ const cors = require('cors')
 
 app.use(cors())
 
-let oldPrices = []
+let oldBtcPairs = []
+let oldBtcUsdt = {}
 
-const storePrices = async () => {
-  const res = await fetch('https://api.binance.com/api/v1/ticker/price')
+const fetchPrices = async () => {
+  const res = await fetch('https://api.binance.com/api/v3/ticker/price')
   console.log('Status: ' + res.status)
   if (res.status === 429) {
     clearInterval(fetchPricesInteval)
     console.log("WARNING - Binance returned 429 (too many requests)!")
   }
   const json = await res.json()
-  addPriceChangeProperty(json)
-  client.setJson('prices', json)
-  oldPrices = json
+  const btcUsdt = json.find(coin => coin.symbol === 'BTCUSDT')
+  const btcPairs = json.filter(coin => coin.symbol.endsWith('BTC'))
+  addPriceChangeProperty(btcPairs)
+  addUsdtPrice(btcPairs, btcUsdt.price)
+  client.setJson('btcpairs', btcPairs)
+  oldBtcPairs = btcPairs
+  btcUsdt.change = btcUsdt.price.localeCompare(oldBtcUsdt.price)
+  client.setJson('btcusdt', btcUsdt)
+  oldBtcUsdt = btcUsdt
 }
 
-const fetchPricesInteval = setInterval(storePrices, 5000)
+const fetchPricesInteval = setInterval(fetchPrices, 1000)
 
-const addPriceChangeProperty = newPrices => {
-  if (oldPrices.length - newPrices.length != 0) {
-    newPrices.forEach(element => {
-      element.change = 0
+const addPriceChangeProperty = newBtcPairs => {
+  if (oldBtcPairs.length != newBtcPairs.length) {
+    newBtcPairs.forEach(coin => {
+      coin.change = 0
     })
     return
   }
-  for (let i = 0; i < oldPrices.length; ++i) {
-    if (newPrices[i].price > oldPrices[i].price) {
-      newPrices[i].change = 1
-    } else if (newPrices[i].price < oldPrices[i].price) {
-      newPrices[i].change = -1
-    } else {
-      newPrices[i].change = 0
-    }
+  for (let i = 0; i < newBtcPairs.length; ++i) {
+    newBtcPairs[i].change = newBtcPairs[i].price.localeCompare(oldBtcPairs[i].price)
   }
 }
 
+const addUsdtPrice = (btcPairs, btcUsdtPrice) => {
+  btcPairs.forEach(coin => {
+    coin.usdt = (coin.price * btcUsdtPrice).toString()
+  })
+}
 
-app.get('/prices', async (req, res) => {
-  res.json(await client.getJson('prices'))
+
+app.get('/btcpairs', async (req, res) => {
+  res.json(await client.getJson('btcpairs'))
+})
+
+app.get('/btcusdt', async (req, res) => {
+  res.json(await client.getJson('btcusdt'))
 })
 
 module.exports = app
